ab7124d1c2f592136240aa6a1724f9d4928ffb02
Lscythe <lscythe.git@proton.me>
2024-11-25 06:19:57 +0700

    feat: update domain setting and add data setting module

--- core/datastore/src/main/kotlin/dev/rendrap/app/movienight/core/datastore/constant/PreferencesKey.kt
+++
@@ -1,3 +0,0 @@
-package dev.rendrap.app.movienight.core.datastore.constant
-
-object PreferencesKey
--- domain/setting/src/main/kotlin/dev/rendrap/app/movienight/domain/setting/repository/SettingRepository.kt
+++ domain/setting/src/main/kotlin/dev/rendrap/app/movienight/domain/setting/repository/SettingRepository.kt
@@ -3,6 +3,8 @@ package dev.rendrap.app.movienight.domain.setting.repository
 import dev.rendrap.app.movienight.core.common.utils.Theme
 
 interface SettingRepository {
-    suspend fun getCurrentLanguage(): Theme
-    suspend fun getCurrentTheme(): String
+    suspend fun getCurrentLanguage(): String
+    suspend fun getCurrentTheme(): Theme
+    suspend fun setCurrentLanguage(language: String)
+    suspend fun setCurrentTheme(theme: Theme)
 }
